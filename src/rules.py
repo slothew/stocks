b78f53a00b6945a7809efae1e74ec9fea873f187
shall.4eva@gmail.com <shall.4eva@gmail.com>
2021-01-05 23:21:21 -0500

    handle exception on bad ticker, comments

--- src/rules.py
+++ src/rules.py
@@ -9,23 +9,33 @@ import yfinance
 def is_buy(ticker: yfinance.Ticker):
     """
     Use this function to return True if the stock looks like a BUY, or False if it doesn't look like a BUY.
+
+    :param ticker: the Ticker object containing information about a stock.
+    :return: True if we should indicate a "buy" signal, False otherwise.
     """
 
     # implement your own, better rules (and check that the examples are what the comments say they are!)
 
-    # if the 52 week trend is positive, buy!
-    if ticker.info["52WeekChange"] > 0:
-        print(
-            f"52 week change of {ticker.info['52WeekChange']} is positive, suggest BUY!"
-        )
-        return True
+    try:
+        # if the 52 week trend is positive, buy!
+        if ticker.info["52WeekChange"] > 0:
+            print(
+                f"52 week change of {ticker.info['52WeekChange']} is positive, suggest BUY!"
+            )
+            return True
 
-    # if the price has declined since market open, buy!
-    if ticker.info["regularMarketOpen"] - ticker.info["regularMarketPrice"] > 0:
-        print(
-            f"Current price of {ticker.info['regularMarketPrice']} is less than the opening price of {ticker.info['regularMarketOpen']}, suggest BUY"
-        )
-        return True
+        # if the price has declined since market open, buy!
+        if ticker.info["regularMarketOpen"] - ticker.info["regularMarketPrice"] > 0:
+            print(
+                f"Current price of {ticker.info['regularMarketPrice']} is less than the opening price of {ticker.info['regularMarketOpen']}, suggest BUY"
+            )
+            return True
+
+    # some cases, such as when a ticker symbol is invalid, produce errors on indexing into 'ticker' objects.
+    except ValueError as e:
+        print(f"Problem examining the ticker: {e}")
+        # don't issue a buy ruling when we had a problem
+        return False
 
     # if we didn't hit any "buy trigger" rules, tell caller we're not interested in buying it.
     return False
